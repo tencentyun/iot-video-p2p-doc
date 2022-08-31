841b725daa40674223915149279d5f721acbedf3
xuanwang <xuanwang@tencent.com>
2022-08-31 13:04:25 +0800

    feat: 开发版才开启logger，并加个会影响性能提示

--- demo/miniprogram/app.js
+++ demo/miniprogram/app.js
@@ -5,8 +5,11 @@ App({
     console.log('App: onLaunch');
 
     this.console = console;
-    this.logger = new Logger({
-      logManagePage: '/pages/xp2p-records/records?name=logs',
-    });
+    if (wx.getAccountInfoSync().miniProgram.envVersion === 'develop') {
+      console.error('【注意】开启 Logger 会影响运行性能，请避免频繁写入');
+      this.logger = new Logger({
+        logManagePage: '/pages/xp2p-records/records?name=logs',
+      });
+    }
   },
 });
